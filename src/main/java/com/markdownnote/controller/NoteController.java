fae2bfd9e805b611b824f437b9731160c5d5f95b
ibenkim <125783238+ibenkim@users.noreply.github.com>
2025-10-01 02:02:54 -0700

    Update NoteController.java

--- src/main/java/com/markdownnote/controller/NoteController.java
+++ src/main/java/com/markdownnote/controller/NoteController.java
@@ -1,1 +1,13 @@
+package com.markdownnote.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoteController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Welcome to MarkdownNote!";
+    }
+}
