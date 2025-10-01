9763a5bc74fec9358f7db01379235080dafb6fab
ibenkim <125783238+ibenkim@users.noreply.github.com>
2025-10-01 02:03:20 -0700

    Update MarkdownNoteApplication.java

--- src/main/java/com/markdownnote/MarkdownNoteApplication.java
+++ src/main/java/com/markdownnote/MarkdownNoteApplication.java
@@ -1,1 +1,11 @@
+package com.markdownnote;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownNoteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarkdownNoteApplication.class, args);
+    }
+}
