c98307af85456eebd97f2ec33ca2c019c6b558a2
ibenkim <125783238+ibenkim@users.noreply.github.com>
2025-11-09 05:19:31 -0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
+import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
